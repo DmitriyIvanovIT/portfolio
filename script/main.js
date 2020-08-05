eb83b77990ecccd646db7198a8568eb25b760da6
DmitriyIvanovIT <strela199720@gmail.com>
2020-08-05 21:30:15 +0300

    Сделана шапка, блок обо мне, блок с работами

--- script/main.js
+++ script/main.js
@@ -1,4 +1,6 @@
-const card = document.querySelectorAll('.card');
+const card = document.querySelectorAll('.card'),
+worksCards = document.querySelector('.works-cards'),
+worksCard = document.querySelectorAll('.works-card');
 
 const cardProgress = item => {
     const progress = item.querySelector('.number h2').textContent,
@@ -6,6 +8,27 @@ const cardProgress = item => {
     
     circle.style.strokeDashoffset = `calc(440 - (440 * ${parseInt(progress)}) / 100)`;
 
+}, 
+closeCard = () => {
+    worksCard.forEach(item => item.classList.remove('active'));
+},
+openCard = elem => {
+    closeCard();
+    elem.classList.add('active');
 }
 
+
 card.forEach(item => cardProgress(item));
+
+document.addEventListener('click', event => {
+    const target = event.target;
+
+    if (target.closest('.works-card')) {
+        const item = target.closest('.works-card');
+
+        openCard(item);
+    } else {
+        closeCard();
+    };
+
+});
